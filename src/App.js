97e423cfeeea4d3959c32b6a78cacf39f23ebaff
JephteyAdolphe <ja983@scarletmail.rutgers.edu>
2020-08-10 02:20:14 -0400

    test?

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 import About from './pages/About';
 import Resume from './pages/Resume';
 import { Link } from 'react-router-dom';
-import { Spring } from 'react-spring';
 
 function App() {
   return (
